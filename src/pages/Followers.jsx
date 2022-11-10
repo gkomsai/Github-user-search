76b28664c40276db2d6160960221aa56c031889a
gkomsai <gkomsai788@gmail.com>
2022-11-11 03:04:54 +0530

    Followers page made responsive

--- src/pages/Followers.jsx
+++ src/pages/Followers.jsx
@@ -1,15 +1,16 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
-
 import FollowersCard from "../components/FollowersCard";
+import { extendTheme } from '@chakra-ui/react'
 
 const Followers = () => {
   const followersData = useSelector((store) => store.followersData);
 
   return (
     <>
-      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        <Heading m="1rem" textAlign={"center"}>Followers</Heading>
+      <SimpleGrid columns={[1, 2, 3]} >
         {followersData?.map((el) => (
           <FollowersCard key={el.id} data={el} />
         ))}
---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+
+const breakpoints = {
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+}
